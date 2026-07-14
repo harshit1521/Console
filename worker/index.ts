7c9e5f90514ac2a55609618706ab519c0bc03ad2
Harshit-Navik <hrxit1984@gmail.com>
2026-07-15 01:01:28 +0530

    remove db dependency and update redis publisher

--- worker/index.ts
+++ worker/index.ts
@@ -19,14 +19,7 @@ await redis.connect()
             const response = await redis.brPop("task", 0);
             if (!response) continue;
 
-            const { submissionId } = JSON.parse(response.element);
-
-            // fetch code from db by using submissionId
-            const submission = await prisma.submission.findUnique({ where: { id: submissionId } })
-            if (!submission) throw new Error("no submission data available...");
-
-            const { code, language } = submission;
-            let Output = "";
+            const { code , language , id } = JSON.parse(response.element);
 
             if (language === "JAVASCRIPT") {
 
@@ -35,17 +28,14 @@ await redis.connect()
                 const __filename = fileURLToPath(import.meta.url);
                 const __dirname = path.dirname(__filename);
 
-                const filePath = path.join(__dirname, "code", `${submissionId}.js`); // create js file for each task
+                const filePath = path.join(__dirname, "code", `${id}.js`); // create js file for each task
                 fs.writeFileSync(filePath, code); // write code to file 
 
-                const channel = `submission:${submissionId}`;
+                const channel = `output:${id}`;
                 const child = spawn("node", [filePath]); // start node process 
-
-                // const timer = setTimeout(() => { child.kill("SIGKILL"); }, 10000); // kill process if blocks thread 
 
                 child.stdout.on("data", async (chunk) => {
 
-                    Output += chunk.toString();
                     console.log(chunk.toString());
                     
                     await redis.publish(channel, JSON.stringify({ type: "stdout", data: chunk.toString() })) // publish each chunk for read
@@ -60,15 +50,6 @@ await redis.connect()
                 await new Promise<void>((resolve) => {
 
                     child.on("close", async (exitCode) => {
-
-                        // clearTimeout(timer); // clears proccess timer killer 
-                        const status = exitCode === 0 ? "SUCCESS" : "INTERNAL_ERROR";
-
-                        // update database
-                        await prisma.submission.update({ 
-                            where: { id: submissionId },
-                            data: { status, output: Output }
-                        })
 
                         console.log(`execution completed ...`);
                         
