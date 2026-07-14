efdb6c634ce12d76f31f0a0a1c4f9ae412c51c27
Harshit-Navik <hrxit1984@gmail.com>
2026-07-14 10:23:29 +0530

    implemented ws connection at port: 8080

--- server/index.ts
+++ server/index.ts
@@ -4,11 +4,16 @@ import cors from "cors";
 import { prisma } from "./db";
 import { createClient } from "redis";
 import { stat } from "node:fs";
+import { WebSocketServer } from "ws";
+import { Socket } from "node:dgram";
 
 const redis = createClient({
     url: process.env.REDIS_URL!,
 });
 await redis.connect();
+
+const subClient = redis.duplicate();
+await subClient.connect();
 
 const app = express();
 
@@ -26,27 +31,59 @@ app.post("/submission", async (req, res) => {
         }
     })
 
-    await redis.lPush("task", JSON.stringify({ submissionId: response.id }));
-
     res.json({
         submissionId: response.id,
         status: response.status
     })
 })
 
-app.get("/submission/:subId/stream", async (req, res) => {
-    
-    const subId = req.params?.subId; 
+const wss = new WebSocketServer({ port: 8080 });
 
-    await redis.subscribe(`submission:${subId}`, (message) => {
+wss.on("connection", async (socket) => {
+    console.log("new client connected");
+
+    let activeSubmissionId: string | null = null;
+    socket.on("message", async (data) => {
+        const submissionId = data.toString().trim();
+        activeSubmissionId = submissionId;
+        console.log(submissionId);
+
         try {
-            let data = JSON.parse(message);
-            console.log(data.data);
+
+            await redis.subscribe(`submission:${submissionId}`, (message) => {
+
+                let response = JSON.parse(message);
+                socket.send(response.data);
+                console.log(response.data);
+
+
+                if (response.type === "done") {
+                    socket.close();
+                }
+
+            })
+
+            await redis.lPush("task", JSON.stringify({ submissionId }));
         } catch (error) {
             console.log(error)
         }
+    });
+
+    socket.on("close", async () => {
+        console.log("WebSocket connection closed, cleaning up Redis client");
+        try {
+
+            subClient.unsubscribe(`submission:${activeSubmissionId}`);
+        } catch (error) {
+            console.error(`Failed to unsubscribe from channel submission:${activeSubmissionId}:`, error);
+        }
     })
+
 })
+
+
+
+
 
 
 app.listen("3000", () => {
