71bf8b8bb9571e9051f024c56aa9d4e2c7d7fe39
Harshit-Navik <hrxit1984@gmail.com>
2026-07-15 11:17:55 +0530

    update redis connec. , add kill process

--- worker/index.ts
+++ worker/index.ts
@@ -1,50 +1,69 @@
 import "dotenv/config"
-import { prisma } from "./db";
-import { createClient } from "redis";
 import fs from "fs";
 import path from "path";
-import { spawn } from "child_process";
 import { fileURLToPath } from "url";
-import { exitCode, stdout } from "process";
+import { createClient } from "redis";
+import { spawn } from "child_process";
 
+// primary redis client for regular operations like , publish , lpush , brpop etc ..
 const redis = createClient({
     url: process.env.REDIS_URL!,
 });
-await redis.connect()
+await redis.connect();
+
+// secondary redis client for subscription only ( no other regular operations are allowed, if its in subscription mode ...)
+const subClient = redis.duplicate();
+await subClient.connect()
     .then(async () => {
         console.log(`worker started !!`);
-        
+
         while (1) {
-            // fetch task from queue 
+
+            // ---------------- fetch task from queue ----------------
             const response = await redis.brPop("task", 0);
             if (!response) continue;
 
-            const { code , language , id } = JSON.parse(response.element);
+            // ---------------- extract data ----------------
+            const { code, language, id } = JSON.parse(response.element);
 
+            // ---------------- language selection & exectution ----------------
             if (language === "JAVASCRIPT") {
 
                 console.log(`started code execution ...`);
-                
+
                 const __filename = fileURLToPath(import.meta.url);
                 const __dirname = path.dirname(__filename);
 
                 const filePath = path.join(__dirname, "code", `${id}.js`); // create js file for each task
                 fs.writeFileSync(filePath, code); // write code to file 
 
-                const channel = `output:${id}`;
+                const inputChannel = `input:${id}`;
+                const outputChannel = `output:${id}`;
                 const child = spawn("node", [filePath]); // start node process 
 
                 child.stdout.on("data", async (chunk) => {
 
                     console.log(chunk.toString());
-                    
-                    await redis.publish(channel, JSON.stringify({ type: "stdout", data: chunk.toString() })) // publish each chunk for read
+                    try {
+
+                        await subClient.subscribe(inputChannel, (message) => {
+                            const response = JSON.parse(message.toString());
+                            if (response.type === "kill") {
+                                child.kill("SIGKILL");
+                            }
+                        })
+                    } catch (error) {
+
+                        console.log(`input channel err: ${error}`);
+                    }
+
+                    await redis.publish(outputChannel, JSON.stringify({ type: "stdout", data: chunk.toString() })); // publish each chunk for read
                 })
 
                 child.stderr.on("data", async (chunk) => {
-                    
+
                     console.log(chunk.toString());
-                    await redis.publish(channel, JSON.stringify({ type: "stderr", data: chunk.toString() })) // publish err for read
+                    await redis.publish(outputChannel, JSON.stringify({ type: "stderr", data: chunk.toString() })); // publish err for read
                 })
 
                 await new Promise<void>((resolve) => {
@@ -52,8 +71,8 @@ await redis.connect()
                     child.on("close", async (exitCode) => {
 
                         console.log(`execution completed ...`);
-                        
-                        await redis.publish(channel, JSON.stringify({ type: "done", data: `Execution completed` }));
+
+                        await redis.publish(outputChannel, JSON.stringify({ type: "done", data: `Execution completed` }));
                         fs.unlinkSync(filePath);
 
                         resolve();
